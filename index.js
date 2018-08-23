aa4b9972f82c704f197e526d47675865b3701666
Albara Darwish <albara.darwish@gmail.com>
2018-08-23 12:47:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function currentLine(line) {
   }
 
   const numbersAndNames = []
-
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
-
   return `The line is currently: ${numbersAndNames.join(', ')}`
 };
 
@@ -16,12 +14,10 @@ function nowServing(line) {
   if (!line.length) {
     return "There is nobody waiting to be served!"
   }
-
   return `Currently serving ${line.shift()}.`
 };
 
 function takeANumber(line, name) {
   line.push(name)
-
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
